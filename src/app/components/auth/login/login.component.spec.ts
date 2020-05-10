5a5d5d9194e4eb76867b4ef74e924a5ac795bce8
gyxman-main <42173229+gyxman-main@users.noreply.github.com>
2020-05-10 21:11:27 +0300

    added test for login component

--- src/app/components/auth/login/login.component.spec.ts
+++ src/app/components/auth/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {LoginComponent} from './login.component';
+import {MockModule} from 'ng-mocks';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {FormControlErrorModule} from '../form-control-error/form-control-error.module';
 
 describe('LoginComponent - компонент входа в приложение', () => {
     let component: LoginComponent;
@@ -9,11 +14,25 @@ describe('LoginComponent - компонент входа в приложение
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [LoginComponent],
+            imports: [
+                MockModule(ReactiveFormsModule),
+                MockModule(MatInputModule),
+                MockModule(MatButtonModule),
+                MockModule(FormControlErrorModule),
+            ],
         }).compileComponents();
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(LoginComponent);
         component = fixture.componentInstance;
+    });
+
+    it('Если компонент загрузился, то все элементы отрисовались', () => {
+        // act
+        fixture.detectChanges();
+
+        // assert
+        expect(fixture.nativeElement).toMatchSnapshot();
     });
 });
