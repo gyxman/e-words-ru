06e69f22a6da465914954114c58b992e38e6e3bf
gyxman-main <42173229+gyxman-main@users.noreply.github.com>
2020-05-25 21:59:07 +0300

    [refactor] changed format localstorage mock file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     preset: 'jest-preset-angular',
-    setupFiles: ['<rootDir>/src/app/testing/localstorage-mock.js'],
+    setupFiles: ['<rootDir>/src/app/testing/localstorage-mock.ts'],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
